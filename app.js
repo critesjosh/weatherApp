5621d6c7da25f69041177febee9a7e2cbbf90196
Josh <critesjosh@gmail.com>
2016-07-08 11:36:45 -0600

    Added display elements
    + Added background images that change based on weather/time of day
    + Added display of weather data

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ var longitude;
 
 var temperatureF;
 var temperatureC;
+var weatherID;
 var currentConditions;
 var cloudiness;
 var locationName;
 
+var currentTime = Math.round(new Date().getTime()/1000);
+var sunrise;
+var sunset;
+
 var setLocation = function(lat, long) {
   latitude = lat;
   longitude = long;
-  console.log(latitude, longitude);
 }
 
 var getWeather = function() {
@@ -19,11 +23,69 @@ var getWeather = function() {
   }).done(function(data){
     console.log(data);
     locationName = data.name;
-    currentConditions = data.weather[0].main;
+    currentConditions = data.weather[0].description;
+    weatherID = data.weather[0].id;
     var temp = data.main.temp;
-    temperatureF = 9 / 5 * (temp - 273) + 32;
-    temperatureC = temp - 273;
+    temperatureF = Math.floor(9 / 5 * (temp - 273) + 32);
+    temperatureC = Math.floor(temp - 273);
     cloudiness = data.clouds.all + '%';
+    sunrise = data.sys.sunrise;
+    sunset = data.sys.sunset;
+    displayWeather();
+  });
+}
+
+var displayWeather = function() {
+  if (weatherID >= 200 && weatherID < 600){
+    //show rain background
+    if (currentTime < sunset){
+      $("body").css('background-image', 'url("images/rainyDay.jpeg")');
+    } else {
+      $("body").css('background-image', 'url("images/rainyNight.jpg")');
+    }
+  } else if (weatherID >= 600 && weatherID < 630) {
+    //show snow background
+    if (currentTime < sunset){
+      $("body").css('background-image', 'url("images/snowyDay.jpeg")');
+    } else {
+      $("body").css('background-image', 'url("images/snowyNight.jpeg")');
+    }
+  } else if (weatherID >= 700 && weatherID < 801) {
+    //show clear background
+    if (currentTime < sunset) {
+      $("body").css('background-image', 'url("images/clearDay.jpeg")');
+    } else {
+      $("body").css('background-image', 'url("images/clearNight.jpeg")');
+    }
+  } else if (weatherID >= 801 && weatherID < 805) {
+    //show cloudy background
+    if (currentTime < sunset){
+      $("body").css('background-image', 'url("images/cloudyDay.jpeg")');
+    } else {
+      $("body").css('background-image', 'url("images/cloudyNight.jpeg")');
+    }
+  } else if (weatherID >= 900) {
+    //show something
+    $("body").css('background-image', 'url("images/storm.jpeg")');
+  }
+
+  $("#weather-info").html('<p>The current weather in ' + locationName + ' is:</p>');
+  $("#weather-info").append('<p id="temp" class="F"></p>');
+  if ($("#temp").hasClass("F")) {
+    $("#temp").text(temperatureF + '\u00B0F');
+  } else {
+    $("#temp").text(temperatureC + '\u00B0C');
+  }
+  $("#weather-info").append('<button id="toggle-temp-units" class="text-center btn btn-default">Switch Units</button>');
+  $("#weather-info").append('<p>' + currentConditions + '</p>');
+
+  $("#toggle-temp-units").click(function(){
+    $("#temp").toggleClass("F");
+    if ($("#temp").hasClass("F")) {
+      $("#temp").text(temperatureF + '\u00B0F');
+    } else {
+      $("#temp").text(temperatureC + '\u00B0C');
+    }
   });
 }
 
